f9ffd2897b59da6d62a5156dc1c8e593a7022dba
Keith <khowling@gmail.com>
2023-12-16 15:29:32 +0000

    fix link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,15 +70,6 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
           title: 'Community',
           items: [
             {
@@ -98,10 +89,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/khowling',
